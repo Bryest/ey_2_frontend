f0c9f36ec0bca87719dae2d6646cdca4d97be14b
RenatoDev <u201511892@upc.edu.pe>
2024-07-09 23:59:29 -0500

    feat: dialogs for Add, Edit and Delete Supplier

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, model, OnInit, signal } from '@angular/core';
 import { SupplierService } from '../../supplier.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Supplier } from '../../models/Supplier';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogModule, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,17 +13,37 @@ import { Supplier } from '../../models/Supplier';
   imports: [
     MatCardModule,
     MatTableModule,
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatDialogModule
+  ],
+  providers: [
+    DatePipe
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
+
 export class DashboardComponent implements OnInit {
+  readonly dialogRef = inject(MatDialog);
+
+
   suppliers: Supplier[] = [];
   token: string | null = '';
-  displayedColumns: string[] = ['businessName', 'tradeName', 'taxId', 'phoneNumber', 'actions'];
+  displayedColumns: string[] = [
+    'businessName',
+    'tradeName',
+    'taxId',
+    'phoneNumber',
+    'email',
+    'website',
+    'physicalAddress',
+    'country',
+    'annualBilling',
+    'lastEdited',
+    'actions'];
 
-  constructor(private supplierService: SupplierService) { }
+  constructor(private supplierService: SupplierService, private datePipe: DatePipe) { }
 
   ngOnInit(): void {
     this.token = localStorage.getItem('token');
@@ -32,4 +54,84 @@ export class DashboardComponent implements OnInit {
       );
     }
   }
+
+  openDeleteDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialogRef.open(DialogDelete, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
+  openEditDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialogRef.open(DialogEdit, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
+  openAddDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialogRef.open(DialogAdd, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
+  formatTimestamp(timestamp: string): string {
+    return this.datePipe.transform(timestamp, 'MMM d, y, h:mm:ss a') || '';
+  }
+}
+
+
+@Component({
+  selector: 'dialog-delete',
+  templateUrl: 'dialog-delete.html',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogDelete {
+  readonly dialogRef = inject(MatDialogRef<DialogDelete>);
+}
+
+@Component({
+  selector: 'dialog-edit',
+  templateUrl: 'dialog-edit.html',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogEdit {
+  readonly dialogRef = inject(MatDialogRef<DialogEdit>);
+}
+
+@Component({
+  selector: 'dialog-add',
+  templateUrl: 'dialog-add.html',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogAdd {
+  readonly dialogRef = inject(MatDialogRef<DialogAdd>);
 }
